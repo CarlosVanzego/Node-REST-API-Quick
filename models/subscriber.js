fbf01990121a9529f2902f0c95a4dd146c75d8e1
Carlos Vanzego <129208650+CarlosVanzego@users.noreply.github.com>
2024-02-05 12:51:04 -0500

    comments subscriber.js

--- models/subscriber.js
+++ models/subscriber.js
@@ -1,14 +1,19 @@
+// Importing mongoose library to interact with MongoDB
 const mongoose = require('mongoose')
 
+// Defining a schema for the subscriber collection in the MongoDB database
 const subscriberSchema = new mongoose.Schema({
+  // Defining the 'name' field with type String, which is required
   name: {
     type: String,
     required: true
   },
+  // Defining the 'subscribedToChannel' field with type String, which is required
   subscribedToChannel: {
     type: String,
     required: true
   },
+  // Defining the 'subscribeDate' field with type Date, which is required and defaults to the current date and time
   subscribeDate: {
     type: Date,
     required: true,
@@ -16,4 +21,5 @@ const subscriberSchema = new mongoose.Schema({
   }
 })
 
+// Exporting the mongoose model named 'subscriber' based on the defined schema
 module.exports = mongoose.model('subscriber', subscriberSchema)
